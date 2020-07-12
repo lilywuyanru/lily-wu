411ff968e29ef271f7f6c3a1ad7d27b6e6bd786c
Lily <lilywuyanru@gmail.com>
2020-07-12 15:31:33 -0400

    Updates

---
+++ precache-manifest.f2f5101b1d68c425df920d9a148cbfb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6339561486a347f929ebb9680bf810e",
+    "url": "/lily-wu/index.html"
+  },
+  {
+    "revision": "22239093e828aaa0a39d",
+    "url": "/lily-wu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "805b0387929d032c8b77",
+    "url": "/lily-wu/static/js/2.3ff3e659.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lily-wu/static/js/2.3ff3e659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22239093e828aaa0a39d",
+    "url": "/lily-wu/static/js/main.0316e5a6.chunk.js"
+  },
+  {
+    "revision": "fb3d2eade04ea754db44",
+    "url": "/lily-wu/static/js/runtime-main.281676c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lily-wu/static/media/logo.5d5d9eef.svg"
+  }
+]);
